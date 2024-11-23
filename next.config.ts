e0614635ebe27b86f35cc9e16ace937cc3c7168b
Vincent Yen <ccyen8358@gmail.com>
2024-11-24 00:02:32 +0800

    feature: set up mantine UI library

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
 };
 
 export default nextConfig;
---
+++ src/app/practice/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@mantine/core";
+
+export default async function PlaygroundPage() {
+  return (
+    <div className="p-5 container max-w-lg">
+      <Button variant="filled">Button</Button>
+    </div>
+  );
+}
